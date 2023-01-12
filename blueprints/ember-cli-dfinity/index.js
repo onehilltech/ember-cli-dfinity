e98e291290a06b0114af95968db1fd8ed6b0b351
James Hill <james@onehilltech.com>
2023-01-12 00:20:40 -0500

    chore: added missing files

---
+++ blueprints/ember-cli-dfinity/index.js
@@ -0,0 +1,24 @@
+/* eslint-env node */
+
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
+
+module.exports = Blueprint.extend ({
+  description: 'An EmberJS add-on for the Internet Computer',
+
+  async afterInstall () {
+    // Update the ember-cli-build.js file to include the correct configuration for Webpack.
+    // This will ensure that the dependencies for @dfinity/* compile correctly.
+
+    await this.insertIntoFile ('ember-cli-build.js', EMBER_CLI_BUILD_CONTENT, {
+      after: 'let app = new EmberAddon(defaults, {\n'
+    });
+  }
+});
+
+const EMBER_CLI_BUILD_CONTENT = `    autoImport: {
+      webpack: {
+        node: {
+          global: true
+        }
+      }
+    },`;
