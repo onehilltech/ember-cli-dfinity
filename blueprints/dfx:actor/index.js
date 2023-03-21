7b2bfebd2a3fd4c4631270bfe82dfcdb188ff5a1
James Hill <james@onehilltech.com>
2023-03-20 21:52:04 -0400

    fix: missing import statements

--- blueprints/dfx:actor/index.js
+++ blueprints/dfx:actor/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const path = require ('path');
 
 module.exports = {
   description:
@@ -12,14 +13,17 @@ module.exports = {
       type: String,
     },
   ],
-  
+
   afterInstall (options) {
-    if (!!options.declaration) {
+    if (options.declaration !== undefined) {
+      // There is a local declaration. We are going to import the declaration into our project
+      // by linking to it. This will help keep the declaration consistent as it changes.
+
       const declaration = options.declaration || options.entity.name;
       const candidFileName = `${declaration}.did.js`;
       const target = path.resolve (options.target, '../declarations', declaration, candidFileName);
       const link = path.resolve (options.target, './app/declarations', candidFileName);
-        
+
       fs.symlinkSync (target, link, "file");
     }
   },
